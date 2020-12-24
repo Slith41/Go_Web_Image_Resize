59bb8fcdeed1248aa93a8209dfee630511854817
Slith41 <nameisconfidentialinformation@gmail.com>
2020-12-25 01:44:53 +0600

    Image(JPEG, PNG formats) resizer

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/nfnt/resize"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+type UploadResponse struct {
+	Path string
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("image")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	imageType := handler.Header.Get("Content-Type")
+
+	switch imageType {
+	case "image/jpeg":
+
+		img, _, err := image.Decode(file)
+		m := resize.Resize(1000, 1000, img, resize.Lanczos3)
+		path := fmt.Sprintf("media/%s", handler.Filename)
+
+		out, err := os.Create(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		// write new image to file
+		jpeg.Encode(out, m, nil)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		data := UploadResponse{Path: path}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data)
+		break
+
+	case "image/png":
+
+		img, _, err := image.Decode(file)
+		m := resize.Resize(1000, 1000, img, resize.Lanczos3)
+		path := fmt.Sprintf("media/%s", handler.Filename)
+
+		out, err := os.Create(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		// write new image to file
+		png.Encode(out, m)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		data := UploadResponse{Path: path}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data)
+		break
+	}
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	fmt.Println(title)
+	t, _ := template.ParseFiles("templates/home.html")
+	t.Execute(w, &Page{Title: "Resizer"})
+}
+
+func setupRoutes() {
+	fs := http.FileServer(http.Dir("./media"))
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/upload", uploadFile)
+	http.Handle("/media/", http.StripPrefix("/media/", fs))
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	setupRoutes()
+}
