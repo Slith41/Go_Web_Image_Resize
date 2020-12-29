60d79057e597e71df87fc2fd77db3520d6b5e60c
Slith41 <nameisconfidentialinformation@gmail.com>
2020-12-29 14:12:41 +0600

    Добавление хэшей, рефакторинг, введение пользователем выходного изображения

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"image"
 	"image/draw"
@@ -32,25 +31,30 @@ type UploadResponse struct {
 	Path string
 }
 
-func getPictureHash(parts [4]string) string { //функция взятия хеша файла. Для того, чтобы в случае необходимости не обрабатывать заново,
-	hashString := strings.Join(parts[:], ":") //а просто вставить ссылку на такой же файл
+type UploadRequest struct {
+	Width  int
+	Height int
+}
+
+func getPictureHash(parts [3]string) string {
+	hashString := strings.Join(parts[:], ":")
 	h := md5.Sum([]byte(hashString))
 	return hex.EncodeToString(h[:])
 }
 
 func doResize(file multipart.File, filename string, width string, height string, imageType string) string {
-	var parts [4]string
+	var parts [3]string
 	parts[0] = filename
 	parts[1] = width
 	parts[2] = height
-	parts[3] = imageType
-	path := fmt.Sprintf("media/%s", filepath.Ext(filename))
+	hash := getPictureHash(parts)
+	path := fmt.Sprintf("media/%s%s", hash, filepath.Ext(filename))
 	if _, err := os.Stat(path); err == nil {
 		return path
 	}
 
-	if imageType == "jpeg" || imageType == "jpg" || imageType == "png" {
-		fmt.Println("doResize")
+	fmt.Println("doResize")
+	if imageType == "jpg" || imageType == "jpeg" || imageType == "png" {
 		img, _, err := image.Decode(file)
 		width64, _ := strconv.ParseUint(width, 10, 32)
 		height64, _ := strconv.ParseUint(height, 10, 32)
@@ -61,28 +65,27 @@ func doResize(file multipart.File, filename string, width string, height string,
 			return path
 		}
 		defer out.Close()
-
 		if imageType == "jpeg" || imageType == "jpg" {
 			jpeg.Encode(out, m, nil)
-
 		} else if imageType == "png" {
 			png.Encode(out, m)
 		}
-
-	} else if imageType == "gif" { // Формат .gif нфходится отдельно, т.к. требует проведения других операций
+	} else if imageType == "gif" {
 		newGifImg := gif.GIF{}
+		width64, _ := strconv.ParseUint(width, 10, 32)
+		height64, _ := strconv.ParseUint(height, 10, 32)
 		gifImg, err := gif.DecodeAll(file)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		for _, img := range gifImg.Image {
-			resizedGifImg := resize.Resize(500, 0, img, resize.Lanczos2)
+			resizedGifImg := resize.Resize(uint(width64), uint(height64), img, resize.Lanczos2)
 			palettedImg := image.NewPaletted(resizedGifImg.Bounds(), img.Palette)
 			draw.FloydSteinberg.Draw(palettedImg, resizedGifImg.Bounds(), resizedGifImg, image.ZP)
 
 			newGifImg.Image = append(newGifImg.Image, palettedImg)
-			newGifImg.Delay = append(newGifImg.Delay)
+			newGifImg.Delay = append(newGifImg.Delay, 25)
 		}
 		out, err := os.Create(path)
 		if err != nil {
@@ -94,63 +97,46 @@ func doResize(file multipart.File, filename string, width string, height string,
 		if err != nil {
 			log.Fatal(err)
 		}
-
 	}
-
 	return path
 }
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("File Upload Endpoint Hit")
 
-	r.ParseMultipartForm(10 << 20)
+	r.ParseMultipartForm(0)
 	file, handler, err := r.FormFile("image")
+	width := r.FormValue("width")
+	height := r.FormValue("height")
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
 		fmt.Println(err)
 		return
 	}
+	imageExtension := handler.Header.Get("Content-type")
 
-	imageType := handler.Header.Get("Content-Type")
+	if imageExtension == "image/jpeg" || imageExtension == "image/jpg" {
+		path := doResize(file, handler.Filename, width, height, "jpeg")
 
-	if imageType == "image/jpeg" || imageType == "image/png" || imageType == "image/gif" {
-		switch imageType {
-		case "image/jpeg":
+		data := UploadResponse{Path: path}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data)
+	} else if imageExtension == "image/png" {
+		path := doResize(file, handler.Filename, width, height, "png")
 
-			doResize(file, handler.Filename, "100", "0", "jpeg")
-			path := fmt.Sprintf("media/%s", filepath.Ext(handler.Filename))
+		data := UploadResponse{Path: path}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data)
 
-			data := UploadResponse{Path: path}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(data)
-			break
+	} else if imageExtension == "image/gif" {
+		path := doResize(file, handler.Filename, width, height, "gif")
 
-		case "image/png":
-
-			doResize(file, handler.Filename, "100", "0", "png")
-			path := fmt.Sprintf("media/%s", filepath.Ext(handler.Filename))
-
-			data := UploadResponse{Path: path}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(data)
-			break
-
-		case "image/gif":
-			doResize(file, handler.Filename, "100", "0", "gif")
-			path := fmt.Sprintf("media/%s", filepath.Ext(handler.Filename))
-
-			data := UploadResponse{Path: path}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(data)
-
-			break
-		}
-
-	} else {
-		fmt.Println(errors.New("Eror.A file should be either png, jpeg or gif"))
+		data := UploadResponse{Path: path}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data)
 	}
 }
 
